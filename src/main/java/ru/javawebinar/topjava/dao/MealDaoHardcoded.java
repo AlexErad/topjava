08bbcf915be45dc7863710e49484cd544d6e9fcc
Alexander Kabanov <akabanov100@gmail.com>
2020-10-06 20:12:40 +0300

    Moving id for HardcodedMealDao to AtomicInteger

--- src/main/java/ru/javawebinar/topjava/dao/MealDaoHardcoded.java
+++ src/main/java/ru/javawebinar/topjava/dao/MealDaoHardcoded.java
@@ -6,20 +6,21 @@ import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MealDaoHardcoded implements MealDao {
     private List<Meal> testMeals;
-    private static int count = 0;
+    private static AtomicInteger count = new AtomicInteger(0);
 
     public MealDaoHardcoded() {
         testMeals = new CopyOnWriteArrayList<>();
-        testMeals.add(new Meal(++count, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500));
-        testMeals.add(new Meal(++count, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000));
-        testMeals.add(new Meal(++count, LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500));
-        testMeals.add(new Meal(++count, LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100));
-        testMeals.add(new Meal(++count, LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000));
-        testMeals.add(new Meal(++count, LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500));
-        testMeals.add(new Meal(++count,LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410));
+        testMeals.add(new Meal(count.incrementAndGet(), LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500));
+        testMeals.add(new Meal(count.incrementAndGet(), LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000));
+        testMeals.add(new Meal(count.incrementAndGet(), LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500));
+        testMeals.add(new Meal(count.incrementAndGet(), LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100));
+        testMeals.add(new Meal(count.incrementAndGet(), LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000));
+        testMeals.add(new Meal(count.incrementAndGet(), LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500));
+        testMeals.add(new Meal(count.incrementAndGet(),LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410));
     }
 
     @Override
@@ -48,7 +49,7 @@ public class MealDaoHardcoded implements MealDao {
 
     @Override
     public void addMeal(Meal meal) {
-        meal.setId(++count);
+        meal.setId(count.incrementAndGet());
         testMeals.add(meal);
     }
 
